8baff0302c2ff459a536ff63393a2a52dc3a8675
Vladislav Sorokin <sorokinvj@gmail.com>
2018-10-29 13:39:25 +0000

    new server (express) with params

--- server.js
+++ server.js
@@ -1,46 +1,37 @@
-const { createServer } = require('http')
-const { parse } = require('url')
+const express = require('express')
 const next = require('next')
-const pathMatch = require('path-match')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const route = pathMatch()
-
-const matchArticle = route('/article/:uid')
-const matchPhoto = route('/photo/:uid')
-const matchVideo = route('/video/:uid')
-const matchTeam = route('/team/:uid')
-
-
 
 app.prepare()
   .then(() => {
-    createServer((req, res) => {
-      const { pathname, query } = parse(req.url, true)
-      const paramsArticle = matchArticle(pathname)
-      const paramsPhoto = matchPhoto(pathname)
-      const paramsVideo = matchVideo(pathname)
-      const paramsTeam = matchTeam(pathname)
+    const server = express()
 
-
-      if (paramsArticle === false || paramsPhoto === false || paramsVideo === false || paramsTeam === false) {
-        handle(req, res)
-        return
-      }
-      // assigning `query` into the params means that we still
-      // get the query string passed to our application
-      // i.e. /blog/foo?show-comments=true
-      app.render(req, res, '/article', Object.assign(paramsArticle, query))
-      app.render(req, res, '/photo', Object.assign(paramsPhoto, query))
-      app.render(req, res, '/video', Object.assign(paramsVideo, query))
-      app.render(req, res, '/team', Object.assign(paramsTeam, query))
-
+    server.get('/article/:uid', (req, res) => {
+      return app.render(req, res, '/article', { uid: req.params.uid })
     })
-      .listen(port, (err) => {
-        if (err) throw err
-        console.log(`> Ready on http://localhost:${port}`)
-      })
+
+    server.get('/photo/:uid', (req, res) => {
+      return app.render(req, res, '/photo', { uid: req.params.uid })
+    })
+
+    server.get('/video/:uid', (req, res) => {
+      return app.render(req, res, '/video', { uid: req.params.uid })
+    })
+
+    server.get('/team/:uid', (req, res) => {
+      return app.render(req, res, '/team', { uid: req.params.uid })
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
   })
