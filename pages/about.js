e3a4cf9324561ab7b6259b4b34a6367f6faa6161
Vladislav Sorokin <sorokinvj@gmail.com>
2018-11-26 21:18:10 +0000

    final fb meta

--- pages/about.js
+++ pages/about.js
@@ -78,21 +78,19 @@ class About extends React.Component {
                     <meta property="og:locale" content="ru_RU" />
                     <meta property="og:locale:alternate" content="en_US" />
                     <title>{this.context.t("Что мы делаем")}</title>
+                    <meta property="og:image"              content={hostName + "/static/wallpaper1.jpg"} />
+                    <meta property="og:url"                content={hostName + "/about"} />
+                    <meta property="og:type"               content="article" />
                 {(typeof fb_locale === 'undefined' || this.props.fb_locale === "ru_RU") && 
                     <Fragment>
-                        <meta property="og:url"                content={hostName + "/about"} />
                         <meta property="og:title"              content="Что мы делаем" />
                         <meta property="og:description"        content="Уникальный для России формат научного центра, занимающегося как фундаментальными исследованиями, так и разработкой устройств, основанных на квантовых эффектах. Занимает лидирующие позиции в научной области, а также в разработке высокотехнологичных коммерческих продуктов." />
-                        <meta property="og:image"              content={hostName + "/static/wallpaper1.jpg"} />
                     </Fragment>
                 }
                 {this.props.fb_locale === "en_US" && 
                     <Fragment>
-                        <meta property="og:url"                content={hostName + "/about"} />
-                        <meta property="og:type"               content="article" />
                         <meta property="og:title"              content="What we do" />
                         <meta property="og:description"        content="Some description" />
-                        <meta property="og:image"              content={hostName + "/static/wallpaper1.jpg"} />
                     </Fragment>
                 }
                 </Head>
