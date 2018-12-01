4258d6f2aafe8b3f8231374d1c905d4b7d8593ee
Vladislav Sorokin <sorokinvj@gmail.com>
2018-12-01 23:32:21 +0000

    minimum polyfills

--- client/polyfills.js
+++ client/polyfills.js
@@ -5,19 +5,19 @@ import repeat from 'core-js/library/fn/string/virtual/repeat'
 
 
 // Add your polyfills
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+// import 'core-js/es6/map';
+// import 'core-js/es6/set';
 
 import 'core-js/fn/object/assign';
-import 'core-js/fn/string/ends-with';
-import 'core-js/fn/string/starts-with';
-import 'core-js/fn/string/includes';
-import 'core-js/fn/array/includes';
-import 'core-js/fn/array/find';
+// import 'core-js/fn/string/ends-with';
+// import 'core-js/fn/string/starts-with';
+// import 'core-js/fn/string/includes';
+// import 'core-js/fn/array/includes';
+// import 'core-js/fn/array/find';
 
-import 'core-js/fn/number/is-finite';
+// import 'core-js/fn/number/is-finite';
 // This files runs at the very beginning (even before React and Next.js core)
-console.log('Load your polyfills')
+// console.log('Load your polyfills')
 
 String.prototype.includes = includes
 String.prototype.repeat = repeat
