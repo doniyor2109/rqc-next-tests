c6b88dbe72365461cc78a975699d268412e130ff
Vladislav Sorokin <sorokinvj@gmail.com>
2018-12-04 13:08:26 +0000

    ссылки на продукты

--- components/products/index.js
+++ components/products/index.js
@@ -5,114 +5,107 @@ import {ArrowButton} from '../shared/ArrowButton'
 const Products = (props, context) => (
     <section className="products">
 
-        <div className="container">
-
-         <p className="main-category">
-              {context.t("Продукты")}
-        </p>
-
-        <div className="columns is-multiline">
-
-            <div className="column is-6-desktop is-10-tablet">
-                <div className="product large">
-                    <div className="columns">
-                        <div className="column is-6-desktop">
-                            <h3>
-                                {context.t("детектор одиночных фотонов")}
-                            </h3>
-                        </div>
-                        <div className="column is-6-desktop">
-                            <div className="img_wrapper">
-                                <img src="/static/products_detector.png" />
+        <div className="container">`
+            <p className="main-category">
+                {context.t("Продукты")}
+            </p>
+            <div className="columns is-multiline">
+                <div className="column is-6-desktop is-10-tablet">
+                    <a href="http://goqrate.com/#photon-detector" target="_blank" rel="noopener noreferrer">
+                        <div className="product large">
+                            <div className="columns">
+                                <div className="column is-6-desktop">
+                                    <h3>
+                                        {context.t("детектор одиночных фотонов")}
+                                    </h3>
+                                </div>
+                                <div className="column is-6-desktop">
+                                    <div className="img_wrapper">
+                                        <img src="/static/products_detector.png" />
+                                    </div>
+                                </div>
                             </div>
+                            <ArrowButton color="040303" />
                         </div>
-                    </div>
-                    <ArrowButton color="040303" />
+                    </a>
+
                 </div>
 
-            </div>
+                <div className="column is-6-desktop is-10-tablet">
+                    <a href="http://www.femtovision.ru" target="_blank" rel="noopener noreferrer">
+                        <div className="product large">
+                            <div className="columns">
+                                <div className="column is-6-desktop">
+                                    <h3>
+                                        {context.t("Femto Vision")}
+                                    </h3>
+                                    <p>
+                                        {context.t("Фемтосекундный лазер")}
+                                    </p>
+                                </div>
+                                <div className="column is-6-desktop">
+                                    <div className="img_wrapper">
+                                        <img src="/static/products_femtovision.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <ArrowButton color="040303" />
+                        </div>
+                    </a>
+                </div>
 
-            <div className="column is-6-desktop is-10-tablet">
-
-                <div className="product large">
-                    <div className="columns">
-                        <div className="column is-6-desktop">
+                <div className="column is-4-desktop is-6-tablet">
+                    {/* <a href="" target="_blank" rel="noopener noreferrer"> */}
+                        <div className="product small granat">
                             <h3>
-                                {context.t("Femto Vision")}
+                                {context.t("М-Гранат")}
                             </h3>
                             <p>
-                                {context.t("Фемтосекундный лазер")}
+                                {context.t("Магнитокардиограф")}
                             </p>
-                        </div>
-                        <div className="column is-6-desktop">
                             <div className="img_wrapper">
-                                <img src="/static/products_femtovision.png" />
+                                <img src="/static/products_granat.png" />
                             </div>
+                            <ArrowButton color="040303" />
                         </div>
-                    </div>
-                    <ArrowButton color="040303" />
-
+                    {/* </a> */}
                 </div>
-            
+
+                <div className="column is-4-desktop is-6-tablet">
+                    <a href="http://goqrate.com" target="_blank" rel="noopener noreferrer">
+                        <div className="product small qrate">
+                            <h3>
+                                {context.t("Q Rate")}
+                            </h3>
+                            <p>
+                                {context.t("Квантовые коммуникации")}
+                            </p>
+                            <div className="img_wrapper">
+                                <img src="/static/products_qrate.png" />
+                            </div>
+                            <ArrowButton color="040303" />
+                        </div>
+                    </a>
+                </div>
+
+                <div className="column is-4-desktop is-6-tablet">
+                    <a href="https://dephandetectors.com/ru/" target="_blank" rel="noopener noreferrer">
+                        <div className="product small dephan">
+                            <h3>
+                                {context.t("Dephan Detectors")}
+                            </h3>
+                            <p>
+                                {context.t("Твердотельный фотоумножитель")}
+                            </p>
+                            <div className="img_wrapper">
+                                <img src="/static/products_dephan.png" />
+                            </div>
+                            <ArrowButton color="040303" />
+                        </div>
+                    </a>
+                </div>       
             </div>
-
-            <div className="column is-4-desktop is-6-tablet">
-                <div className="product small granat">
-                    <h3>
-                        {context.t("М-Гранат")}
-                    </h3>
-                    <p>
-                        {context.t("Магнитокардиограф")}
-                    </p>
-                    <div className="img_wrapper">
-                        <img src="/static/products_granat.png" />
-                    </div>
-                    <ArrowButton color="040303" />
-
-                </div>
-            </div>
-
-            <div className="column is-4-desktop is-6-tablet">
-
-                <div className="product small qrate">
-                    <h3>
-                        {context.t("Q Rate")}
-                    </h3>
-                    <p>
-                        {context.t("Квантовые коммуникации")}
-                    </p>
-                    <div className="img_wrapper">
-                        <img src="/static/products_qrate.png" />
-                    </div>
-                    <ArrowButton color="040303" />
-
-                </div>
-            
-            </div>
-
-            <div className="column is-4-desktop is-6-tablet">
-
-                <div className="product small dephan">
-                    <h3>
-                        {context.t("Dephan Detectors")}
-                    </h3>
-                    <p>
-                        {context.t("Твердотельный фотоумножитель")}
-                    </p>
-                    <div className="img_wrapper">
-                        <img src="/static/products_dephan.png" />
-                    </div>
-                    <ArrowButton color="040303" />
-                </div>
-            
-            </div>       
         </div>
-
-
-        
-        </div>
-
-
     </section>
 )
 
