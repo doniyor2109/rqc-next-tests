46dd1dbb61f89f9724f9abd8b007bd7ac72f9024
Vladislav Sorokin <sorokinvj@gmail.com>
2019-01-23 09:33:31 +0000

    delete prefetch

--- components/navbar/NavbarMenuDesktop.js
+++ components/navbar/NavbarMenuDesktop.js
@@ -13,7 +13,7 @@ const NavbarMenuDesktop = (props, context) => {
   const { Menu, withSlider, switchLanguage, currentLanguage, searchClick } = props
   const items = Menu.map((item, key) =>
 
-    <Link href={item.url} key={key} prefetch>
+    <Link href={item.url} key={key}>
       <a className={(withSlider ? "is-white opacity080" : "is-black opacity050") + " navbar-item"}>
         {context.t(item.name)}
       </a>
