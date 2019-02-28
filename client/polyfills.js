7e084e1648effcb7bd2a1a9049aed81a4bc74457
Vladislav Sorokin <sorokinvj@gmail.com>
2019-02-28 19:10:29 +0000

    new polyfills

--- client/polyfills.js
+++ client/polyfills.js
@@ -5,14 +5,14 @@ import repeat from 'core-js/library/fn/string/virtual/repeat'
 
 
 // Add your polyfills
-// import 'core-js/es6/map';
-// import 'core-js/es6/set';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 
 import 'core-js/fn/object/assign';
 // import 'core-js/fn/string/ends-with';
 // import 'core-js/fn/string/starts-with';
-// import 'core-js/fn/string/includes';
-// import 'core-js/fn/array/includes';
+import 'core-js/fn/string/includes';
+import 'core-js/fn/array/includes';
 // import 'core-js/fn/array/find';
 
 // import 'core-js/fn/number/is-finite';
