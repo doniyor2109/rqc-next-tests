2917b75d5784b5f61c36eda3a88eacf0a168895a
Vladislav Sorokin <sorokinvj@gmail.com>
2019-02-28 20:34:34 +0000

    includes

--- client/polyfills.js
+++ client/polyfills.js
@@ -1,7 +1,7 @@
 /* eslint no-extend-native: 0 */
 // core-js comes with Next.js. So, you can import it like below
-// import includes from 'core-js/library/fn/string/virtual/includes'
-// import repeat from 'core-js/library/fn/string/virtual/repeat'
+import includes from 'core-js/library/fn/string/virtual/includes'
+import repeat from 'core-js/library/fn/string/virtual/repeat'
 
 
 // Add your polyfills
