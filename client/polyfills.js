b4c4a094e90a95d4e53963d545094bf4aa7992f5
Vladislav Sorokin <sorokinvj@gmail.com>
2019-02-28 20:35:58 +0000

    includes array

--- client/polyfills.js
+++ client/polyfills.js
@@ -12,7 +12,7 @@ import 'core-js/fn/object/assign';
 // import 'core-js/fn/string/ends-with';
 // import 'core-js/fn/string/starts-with';
 // import 'core-js/fn/string/includes';
-// import 'core-js/fn/array/includes';
+import 'core-js/fn/array/includes';
 // import 'core-js/fn/array/find';
 
 // import 'core-js/fn/number/is-finite';
