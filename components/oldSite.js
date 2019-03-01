894c19a1ff54a6b9d8ba6e113c2044f4b8a864d9
Vladislav Sorokin <sorokinvj@gmail.com>
2019-03-01 12:36:45 +0000

    oldsite fix

--- components/oldSite.js
+++ components/oldSite.js
@@ -6,14 +6,16 @@ const OldSite = (props, context) => (
     <div className="old-site-popup">
         <div className="container">
             <div className="columns">
-                <div className="column is-7-desktop is-offset-1-desktop is-8-tablet">
+                <div className="column is-7-desktop is-offset-1-desktop is-7-tablet">
                     <p>
                         {context.t("Добро пожаловать на новый сайт РКЦ! Мы все еще работаем над его наполнением. Если вы не можете найти какую-то информацию, посмотрите на нашем старом сайте.")}
                     </p>
                 </div>
-                <div className="column is-3-desktop">
+                <div className="column is-4-desktop is-5-tablet">
                     <a href="http://prev.rqc.ru" target="_blank" rel="noopener noreferrer">
-                        {context.t("Перейти на старый сайт")}
+                        <button>
+                            {context.t("Перейти на старый сайт")}
+                        </button>
                     </a>      
                 </div> 
             </div>     
