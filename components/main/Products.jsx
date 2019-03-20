3623433692c62b1ccf63f5ca5a6c6dafc2f726c4
Vladislav Sorokin <sorokinvj@gmail.com>
2019-03-20 22:58:37 +0000

    product style fix

--- components/main/Products.jsx
+++ components/main/Products.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Media from 'react-media'
-import styled from 'styled-components'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import {ArrowButton} from '../shared/ArrowButton'
+import ProductType from './ProductType';
 import ProductCard from './ProductCard';
 
 
-const Products = ({items, phone, tablet, className}, { t }) => (
-    <section className={className} id="products">
-        <div className="container">
-            <p className="main-category">
-                {t("Продукты")}
-            </p>
-            <div className="columns is-multiline">
-                    {items.map((item, index) => 
-                        <ProductCard item={item} key={item.id} big={index < 2}/>
-                    )}
-            </div>
-        </div>
-    </section>
-)
+const Products = ({
+  items, className,
+}, { t }) => (
+  <section className={className} id="products">
+    <div className="container">
+      <p className="main-category">
+        {t('Продукты')}
+      </p>
+      <div className="columns is-multiline">
+        {items.map((item, index) => <ProductCard item={item} key={item.id} big={index < 2} />)}
+      </div>
+    </div>
+  </section>
+);
 
 const StyledProducts = styled(Products)`
     padding: 6rem 0 8rem;
     margin-top: 7rem;
     background: #F7F9FB;
-`
+`;
 
 Products.contextTypes = {
-    t: PropTypes.func
-}
-  
+  t: PropTypes.func,
+};
 
-export default StyledProducts
+Products.propTypes = {
+  items: PropTypes.arrayOf(ProductType),
+  className: PropTypes.string.isRequired,
+};
+
+Products.defaultProps = {
+  items: [],
+};
+
+export default StyledProducts;
