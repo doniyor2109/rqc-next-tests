8722ad2537e069598cd94ac148229d7acd460fc1
Vladislav Sorokin <sorokinvj@gmail.com>
2019-04-04 12:38:47 +0100

    new ArrowButton with url

--- components/shared/ArrowButton.jsx
+++ components/shared/ArrowButton.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Link from 'next/link';
 
 const Button = styled.button`
   height: 3.5rem;
@@ -54,23 +56,41 @@ const Button = styled.button`
 }
 `;
 
-const ArrowButton = ({ text, color, onClick }) => (
-  <Button color={color} text={text} onClick={onClick}>
-    {text}
-    <img src={`/static/arrow_${color}.svg`} alt="Стрелка для кнопки" />
-  </Button>
-);
+const ArrowButton = ({
+  text, color, onClick, url,
+}) => {
+  if (url) {
+    return (
+      <Link href={url}>
+        <a target="_blank" rel="noopener noreferrer">
+          <Button color={color} text={text} onClick={onClick}>
+            {text}
+            <img src={`/static/arrow_${color}.svg`} alt="Стрелка для кнопки" />
+          </Button>
+        </a>
+      </Link>
+    );
+  }
+  return (
+    <Button color={color} text={text} onClick={onClick}>
+      {text}
+      <img src={`/static/arrow_${color}.svg`} alt="Стрелка для кнопки" />
+    </Button>
+  );
+};
 
 ArrowButton.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
   onClick: PropTypes.func,
+  url: PropTypes.string,
 };
 
 ArrowButton.defaultProps = {
   text: '',
   color: 'fff',
   onClick: () => {},
+  url: '',
 };
 
 export default ArrowButton;
