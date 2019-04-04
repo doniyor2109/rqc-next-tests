a1d9e74ca0e21b58cb0e28a89824a683fb8dcf27
Vladislav Sorokin <sorokinvj@gmail.com>
2019-04-04 12:46:07 +0100

    reports fix

--- components/reports/ReportCard.jsx
+++ components/reports/ReportCard.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { RichText, Link } from 'prismic-reactjs';
+import { RichText } from 'prismic-reactjs';
+import Link from 'next/link';
 import PrismicConfig from '../../prismic-configuration';
 import ArrowButton from '../shared/ArrowButton';
 
@@ -7,14 +9,17 @@ const ReportCard = ({ item }) => {
   const back = { background: `url(${item.wallpaper.url})`, backgroundSize: 'cover' };
   return (
     <div className="column is-12-mobile is-6-tablet is-4-desktop">
-      <div className="report" style={back}>
-        {RichText.render(item.heading, PrismicConfig.linkResolver)}
-        <ArrowButton
-          url={Link.url(item.report_url, PrismicConfig.linkResolver)}
-          color="ffffff"
-          target_blank
-        />
-      </div>
+      <Link href={item.report_url.url}>
+        <a target="_blank" rel="noopener noreferrer">
+          <div className="report" style={back}>
+            {RichText.render(item.heading, PrismicConfig.linkResolver)}
+            <ArrowButton
+              color="ffffff"
+              target_blank
+            />
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
