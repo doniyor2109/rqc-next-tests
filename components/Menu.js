141012a3eb4b28a09b9600b18b4df1506b4061bf
Vladislav Sorokin <sorokinvj@gmail.com>
2019-04-04 13:18:45 +0100

    new footer (needs refactor)

--- components/Menu.js
+++ components/Menu.js
@@ -1,110 +1,107 @@
 const Menu = [
   {
-    name: "О нас",
-    url: "/about",
+    name: 'О нас',
+    url: '/about',
     children: [
       {
-        name: "Что мы делаем",
-        url: "/about#what-we-do"
+        name: 'Что мы делаем',
+        url: '/about#what-we-do',
       },
       {
-        name: "Годовые отчеты",
-        url: "/reports"
+        name: 'Годовые отчеты',
+        url: '/reports',
       },
       {
-        name: "Работа у нас",
-        url: "/about#vacancies"
+        name: 'Работа у нас',
+        url: '/about#vacancies',
       },
       {
-        name: "Наши партнеры",
-        url: "/about#partners"
+        name: 'Наши партнеры',
+        url: '/about#partners',
       },
       {
-        name: "Как нас найти",
-        url: "/about#contact"
+        name: 'Как нас найти',
+        url: '/about#contact',
       },
       {
-        name: "Медиа-кит",
-        url: "/mediakit"
-      }
-    ]
+        name: 'Медиа-кит',
+        url: '/mediakit',
+      },
+    ],
   },
   {
-    name: "Люди",
-    url: "/people",
+    name: 'Люди',
+    url: '/people',
     children: [
       {
-        name: "Руководство",
-        url: "/people#board"
+        name: 'Руководство',
+        url: '/people#board',
       },
       {
-        name: "Международный консультативный совет",
-        url: "/people#international-board"
+        name: 'Международный консультативный совет',
+        url: '/people#international-board',
       },
       {
-        name: "Попечительский совет",
-        url: "/people#board-of-trustees"
-      }
-    ]
+        name: 'Попечительский совет',
+        url: '/people#board-of-trustees',
+      },
+    ],
   },
   {
-    name: "Исследования",
-    url: "/research",
+    name: 'Исследования',
+    url: '/research',
     children: [
       {
-        name: "Научные группы",
-        url: "/research#groups"
+        name: 'Научные группы',
+        url: '/research#groups',
       },
-      // {
-      //   name: "Лаборатории",
-      //   url: "/labs"
-      // },
-      // {
-      //   name: "Внешние исследователи",
-      //   url: "/scientists"
-      // },
       {
-        name: "Публикации",
-        url: "/publications"
-      }
-    ]
+        name: 'Публикации',
+        url: '/publications',
+      },
+    ],
   },
   {
-    name: "Продукты",
-    url: "/#products",
+    name: 'Образование',
+    url: '/education',
+    children: [],
+  },
+  {
+    name: 'Продукты',
+    url: '/#products',
     children: [
       {
-        name: "Квантовые коммуникации",
-        url: "http://goqrate.com"
+        name: 'Квантовые коммуникации',
+        url: 'http://goqrate.com',
       },
       {
-        name: "Детектор одиночных фотонов",
-        url: "http://goqrate.com/#photon-detector"
+        name: 'Детектор одиночных фотонов',
+        url: 'http://goqrate.com/#photon-detector',
       },
       {
-        name: "Магнитометр",
-        url: "/#products"
+        name: 'Магнитометр',
+        url: '/#products',
       },
       {
-        name: "Твердотельный фотоумножитель",
-        url: "https://www.dephan.com/"
+        name: 'Твердотельный фотоумножитель',
+        url: 'https://www.dephan.com/',
       },
       {
-        name: "Фемтосекундный лазер с диодной накачкой",
-        url: "http://femtovision.ru/page5006824.html"
-      }
-    ]
+        name: 'Фемтосекундный лазер с диодной накачкой',
+        url: 'http://femtovision.ru/page5006824.html',
+      },
+    ],
   },
   {
-    name: "Новости",
-    url: "/news",
-    children: []
+    name: 'Новости',
+    url: '/news',
+    children: [],
   },
   {
-    name: "Мероприятия",
-    url: "/events",
-    children: []
-  }
+    name: 'Мероприятия',
+    url: '/events',
+    children: [],
+  },
 ];
 
 export default Menu;
