65601cebe0e8f35b44093e78fca44c338bc5fc32
Vladislav Sorokin <sorokinvj@gmail.com>
2019-04-19 17:56:32 +0100

    polyfills

--- next.config.js
+++ next.config.js
@@ -1,34 +1,34 @@
+/* eslint-disable no-param-reassign */
 // next.config.js
-const withSass = require('@zeit/next-sass')
+const withSass = require('@zeit/next-sass');
+
 module.exports = withSass({
-    webpack: function (config) {
-        const originalEntry = config.entry
-        
-        config.entry = async () => {
-          const entries = await originalEntry()
-    
-          if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
-            entries['main.js'].unshift('./client/polyfills.js')
-          }
-    
-          return entries
-        }
+  webpack(config) {
+    const originalEntry = config.entry;
 
-        config.module.rules.push({
-          test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-          use: {
-            loader: 'url-loader',
-            options: {
-              limit: 100000,
-              name: '[name].[ext]'
-            }
-          }
-        })
-        config.node = {
-          fs: 'empty'
-        }
-        return config
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
+        entries['main.js'].unshift('./client/polyfills.js');
       }
-})
 
+      return entries;
+    };
 
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]',
+        },
+      },
+    });
+    config.node = {
+      fs: 'empty',
+    };
+    return config;
+  },
+});
