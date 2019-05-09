909f3decb0426429024499540de198f322812710
Vladislav Sorokin <sorokinvj@gmail.com>
2019-05-09 18:00:43 +0100

    new loading with timeout

--- components/shared/loading.jsx
+++ components/shared/loading.jsx
@@ -2,30 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const propTypes = {
-  className: PropTypes.string.isRequired,
-};
-
-const Loading = ({ className }) => (
-
-  <div className={className}>
-    <img src="/static/RQCloader2.gif" alt="loading animaton" />
-  </div>
-);
-
-Loading.propTypes = propTypes;
-
-const StyledLoading = styled(Loading)`
+const Styled = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background: white;
     z-index: 100;
 
     img {
       width: 10rem;
     }
-}
 `;
 
-export default StyledLoading;
+class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayLoading: false,
+    };
+    this.enableLoading = this.enableLoading.bind(this);
+    this.timer = setTimeout(this.enableLoading, 250);
+  }
+
+  enableLoading(){
+    this.setState({
+      displayLoading: true,
+    });
+  }
+
+  render() {
+    const { displayLoading } = this.state;
+    if (!displayLoading) {
+      return null;
+    }
+    return (
+      <Styled>
+        <img src="/static/RQCloader.gif" alt="loading animaton" />
+      </Styled>
+    );
+  }
+}
+
+export default Loading;
