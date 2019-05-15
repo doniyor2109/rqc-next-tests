eb61b53fa11fe8a44369c767f91af849760bbabe
Vladislav Sorokin <sorokinvj@gmail.com>
2019-05-15 21:54:51 +0100

    head fix description t

--- components/GeneralHead.jsx
+++ components/GeneralHead.jsx
@@ -5,7 +5,6 @@ const GeneralHead = () => (
   <Head>
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <meta name="description" content="Добро пожаловать на официальный сайт Российского Квантового Центра!" />
     <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
