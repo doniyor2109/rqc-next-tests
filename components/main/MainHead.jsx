362ffa29bb5c995e010b1abb836a8f2f16ad56ec
Vladislav Sorokin <sorokinvj@gmail.com>
2019-08-05 20:27:23 +0100

    meta fix

--- components/main/MainHead.jsx
+++ components/main/MainHead.jsx
@@ -10,13 +10,13 @@ const MainHead = ({ fb_locale }, { t }) => (
     <meta property="og:url" content={hostName} />
     <meta property="og:type" content="article" />
     <meta property="og:locale:alternate" content="en_US" />
+    <meta property="og:image" content={`${hostName}/static/RQClogo_black_ru.png`} />
     {(typeof fb_locale === 'undefined' || fb_locale === 'ru_RU')
         && (
           <Fragment>
             <meta property="og:locale" content="ru_RU" />
             <meta property="og:title" content="Российский Квантовый Центр" />
             <meta property="og:description" content="Добро пожаловать на официальный сайт Российского Квантового Центра!" />
-            <meta property="og:image" content={`${hostName}/static/RQClogo_black_ru.png`} />
           </Fragment>
         )
     }
@@ -26,7 +26,6 @@ const MainHead = ({ fb_locale }, { t }) => (
           <meta property="og:locale" content="en_US" />
           <meta property="og:title" content="Russian Quantum Center – home page" />
           <meta property="og:description" content="Welcome to the official website of the Russian Quantum Center!" />
-          <meta property="og:image" content={`${hostName}/static/RQClogo_black_en.png`} />
         </Fragment>
         )
     }
