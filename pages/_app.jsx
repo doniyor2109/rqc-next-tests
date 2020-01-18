babe102d7dc34095a77d0b2439f1b6fa65270a3d
Vladislav Sorokin <sorokinvj@gmail.com>
2020-01-18 15:33:54 +0000

    english (prismic) vacancies fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,44 +1,43 @@
-import App, { Container } from 'next/app';
-import React from 'react';
-import { Provider } from 'react-redux';
-import cookies from 'next-cookies';
-import Router from 'next/router';
-import I18n from 'redux-i18n';
-import MobileDetect from 'mobile-detect';
-import withReduxStore from '../lib/with-redux-store';
-import Nav from '../components/navbar/Nav';
-import Footer from '../components/footer/Footer';
-import GeneralHead from '../components/GeneralHead';
-import LoadingFull from '../components/shared/loadingFull';
-import Menuloader from '../components/shared/Menuloader';
+import App, { Container } from 'next/app'
+import React from 'react'
+import { Provider } from 'react-redux'
+import cookies from 'next-cookies'
+import Router from 'next/router'
+import I18n from 'redux-i18n'
+import MobileDetect from 'mobile-detect'
+import withReduxStore from '../lib/with-redux-store'
+import Nav from '../components/navbar/Nav'
+import Footer from '../components/footer/Footer'
+import GeneralHead from '../components/GeneralHead'
+import LoadingFull from '../components/shared/loadingFull'
+import Menuloader from '../components/shared/Menuloader'
 
+import { translations } from '../i18n/translations'
 
-import { translations } from '../i18n/translations';
-
-import '../scss/index.scss';
+import '../scss/index.scss'
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+    let pageProps = {}
 
-    const { useragreedwithcookies } = cookies(ctx);
-    const hasLanguageSettingInCookies = cookies(ctx).language;
-    const language = hasLanguageSettingInCookies || 'ru';
+    const { useragreedwithcookies } = cookies(ctx)
+    const hasLanguageSettingInCookies = cookies(ctx).language
+    const language = hasLanguageSettingInCookies || 'ru'
 
     // добываем значение языка из пользовательских кукис
-    const cookieConsent = typeof useragreedwithcookies !== 'undefined';
+    const cookieConsent = typeof useragreedwithcookies !== 'undefined'
 
     // определяем тип устройства, чтобы потом react-media
     // рендерила именно ту версию компонента, которая
     // совпадает с серверным html
-    const { req } = ctx;
-    const md = new MobileDetect(req ? req.headers['user-agent'] : '');
-    const phone = md.phone();
-    const tablet = md.tablet();
+    const { req } = ctx
+    const md = new MobileDetect(req ? req.headers['user-agent'] : '')
+    const phone = md.phone()
+    const tablet = md.tablet()
 
     // Компонент получает свои pageProps с сервера
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+      pageProps = await Component.getInitialProps(ctx)
     }
     // возвращаем pageProps с сервера и значение языка
     return {
@@ -48,20 +47,20 @@ class MyApp extends App {
       tablet,
       hasLanguageSettingInCookies,
       cookieConsent,
-    };
+    }
   }
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       loadingIsActive: false,
-    };
+    }
   }
 
   componentDidMount() {
     // выставляем куки, если их не было
     if (!this.props.hasLanguageSettingInCookies) {
-      document.cookie = `language=${this.props.language};Expires=Wed, 22 Oct 2025 07:28:00 GMT`;
+      document.cookie = `language=${this.props.language};Expires=Wed, 22 Oct 2025 07:28:00 GMT`
     }
 
     // при переходе со страницы на страницу сообщаем компоненту,
@@ -69,40 +68,40 @@ class MyApp extends App {
     Router.events.on('routeChangeStart', () => {
       this.setState({
         loadingIsActive: true,
-      });
-    });
+      })
+    })
     Router.events.on('routeChangeComplete', () => {
       this.setState({
         loadingIsActive: false,
-      });
-    });
+      })
+    })
   }
 
   render() {
     const {
-      Component, pageProps, reduxStore, language, phone, tablet, cookieConsent,
-    } = this.props;
+      Component,
+      pageProps,
+      reduxStore,
+      language,
+      phone,
+      tablet,
+      cookieConsent,
+    } = this.props
 
-    const { loadingIsActive } = this.state;
+    const { loadingIsActive } = this.state
     return (
-      <Container>
-        <Provider store={reduxStore}>
-          <I18n translations={translations} initialLang={language}>
-            <Menuloader />
-            <GeneralHead />
-            {loadingIsActive && <LoadingFull /> }
-            <Nav
-              cookieConsent={cookieConsent}
-              tablet={tablet}
-              phone={phone}
-            />
-            <Component {...pageProps} phone={phone} tablet={tablet} />
-            <Footer />
-          </I18n>
-        </Provider>
-      </Container>
-    );
+      <Provider store={reduxStore}>
+        <I18n translations={translations} initialLang={language}>
+          <Menuloader />
+          <GeneralHead />
+          {loadingIsActive && <LoadingFull />}
+          <Nav cookieConsent={cookieConsent} tablet={tablet} phone={phone} />
+          <Component {...pageProps} phone={phone} tablet={tablet} />
+          <Footer />
+        </I18n>
+      </Provider>
+    )
   }
 }
 
-export default withReduxStore(MyApp);
+export default withReduxStore(MyApp)
