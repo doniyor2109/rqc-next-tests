37f8efeeb425cbb34f868ebf81d85821b375b035
Vladislav Sorokin <sorokinvj@gmail.com>
2020-01-24 11:01:32 +0000

    fix mediakit with arrow function

--- components/mediakit/Videos.jsx
+++ components/mediakit/Videos.jsx
@@ -1,47 +1,47 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import Loading from '../shared/loading';
-import VideoCard from './VideoCard';
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import Loading from '../shared/loading'
+import VideoCard from './VideoCard'
 
 class Videos extends React.Component {
   state = {
     // eslint-disable-next-line react/destructuring-assignment
     itemsNumber: this.props.initialItems,
-  };
+  }
 
   componentDidMount() {
-    const { fetchItems, lang, initialItems } = this.props;
-    fetchItems(lang, initialItems);
+    const { fetchItems, lang, initialItems } = this.props
+    fetchItems(lang, initialItems)
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const { fetchItems, lang } = this.props;
-    const { itemsNumber } = this.state;
+    const { fetchItems, lang } = this.props
+    const { itemsNumber } = this.state
     if (itemsNumber !== prevState.itemsNumber) {
-      fetchItems(lang, itemsNumber);
+      fetchItems(lang, itemsNumber)
     }
 
     // обработка смены языка
     if (lang !== prevProps.lang) {
-      fetchItems(lang, itemsNumber);
+      fetchItems(lang, itemsNumber)
     }
   }
 
-  getMoreVideos() {
-    const { morenews } = this.props;
-    const { itemsNumber } = this.state;
+  getMoreVideos = () => {
+    const { morenews } = this.props
+    const { itemsNumber } = this.state
     this.setState({
       itemsNumber: itemsNumber + morenews,
-    });
+    })
   }
 
   render() {
-    const {
-      items, isFetching, nextPage, initialItems,
-    } = this.props;
-    const { itemsNumber } = this.state;
+    const { items, isFetching, nextPage, initialItems } = this.props
+    const { itemsNumber } = this.state
 
-    if (initialItems === itemsNumber && isFetching) { return <Loading />; }
+    if (initialItems === itemsNumber && isFetching) {
+      return <Loading />
+    }
     return (
       <Fragment>
         <div className="columns is-multiline">
@@ -52,49 +52,52 @@ class Videos extends React.Component {
         <div className="more-news">
           <div className="columns">
             <div className="column is-centered">
-              {initialItems < itemsNumber
-                  && isFetching && <Loading />}
+              {initialItems < itemsNumber && isFetching && <Loading />}
               <hr />
               {nextPage && (
-              // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
-              <img
-                className="more"
-                alt="show more news"
-                onClick={this.getMoreVideos}
-                src="/static/more.svg"
-              />
+                // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+                <img
+                  className="more"
+                  alt="show more news"
+                  onClick={this.getMoreVideos}
+                  src="/static/more.svg"
+                />
               )}
             </div>
           </div>
         </div>
       </Fragment>
-    );
+    )
   }
 }
 
 Videos.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape({
-    uid: PropTypes.string,
-    data: PropTypes.shape({
-      youtube_link: PropTypes.shape({
-        html: PropTypes.string,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      uid: PropTypes.string,
+      data: PropTypes.shape({
+        youtube_link: PropTypes.shape({
+          html: PropTypes.string,
+        }),
+        title: PropTypes.arrayOf(
+          PropTypes.shape({
+            text: PropTypes.string,
+          })
+        ),
       }),
-      title: PropTypes.arrayOf(PropTypes.shape({
-        text: PropTypes.string,
-      })),
-    }),
-  })),
+    })
+  ),
   initialItems: PropTypes.number.isRequired,
   morenews: PropTypes.number.isRequired,
   nextPage: PropTypes.string,
   isFetching: PropTypes.bool.isRequired,
   lang: PropTypes.string.isRequired,
   fetchItems: PropTypes.func.isRequired,
-};
+}
 
 Videos.defaultProps = {
   items: [],
   nextPage: null,
-};
+}
 
-export default Videos;
+export default Videos
