f19c4dd5578f0a29e5a565f8a60baba275c5bbe3
Vladislav Sorokin <sorokinvj@gmail.com>
2020-02-01 16:54:47 +0000

    fixed (#27)

--- components/main/MainSlider.jsx
+++ components/main/MainSlider.jsx
@@ -1,43 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Slider from 'react-slick';
-import { Link, RichText } from 'prismic-reactjs';
-import Media from 'react-media';
-import PrismicConfig from '../../prismic-configuration';
-import { Button } from '../shared/Button';
-import SliderSection from './styled/SliderSection';
-import MainSlide from './styled/MainSlide';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Slider from 'react-slick'
+import { Link, RichText } from 'prismic-reactjs'
+import PrismicConfig from '../../prismic-configuration'
+import { Button } from '../shared/Button'
+import SliderSection from './styled/SliderSection'
+import MainSlide from './styled/MainSlide'
 
 class MainSlider extends React.Component {
   static propTypes = {
     phone: PropTypes.string,
     tablet: PropTypes.string,
-    slides: PropTypes.arrayOf(PropTypes.shape({
-      primary: PropTypes.shape({
-        button_text_slide: PropTypes.string,
-        button_url_slide: PropTypes.shape({
-          url: PropTypes.string,
-        }),
-        description_slide: PropTypes.arrayOf(PropTypes.shape({
-          text: PropTypes.string,
-        })),
-        title_slideh1: PropTypes.arrayOf(PropTypes.shape({
-          text: PropTypes.string,
-        })),
-        title_slideh2: PropTypes.arrayOf(PropTypes.shape({
-          text: PropTypes.string,
-        })),
-        image_slide: PropTypes.shape({
-          url: PropTypes.string,
-          iphone: PropTypes.shape({
+    slides: PropTypes.arrayOf(
+      PropTypes.shape({
+        primary: PropTypes.shape({
+          button_text_slide: PropTypes.string,
+          button_url_slide: PropTypes.shape({
             url: PropTypes.string,
           }),
-          ipad: PropTypes.shape({
+          description_slide: PropTypes.arrayOf(
+            PropTypes.shape({
+              text: PropTypes.string,
+            })
+          ),
+          title_slideh1: PropTypes.arrayOf(
+            PropTypes.shape({
+              text: PropTypes.string,
+            })
+          ),
+          title_slideh2: PropTypes.arrayOf(
+            PropTypes.shape({
+              text: PropTypes.string,
+            })
+          ),
+          image_slide: PropTypes.shape({
             url: PropTypes.string,
+            iphone: PropTypes.shape({
+              url: PropTypes.string,
+            }),
+            ipad: PropTypes.shape({
+              url: PropTypes.string,
+            }),
           }),
         }),
-      }),
-    })),
+      })
+    ),
   }
 
   static defaultProps = {
@@ -47,9 +54,9 @@ class MainSlider extends React.Component {
   }
 
   constructor(props) {
-    super(props);
-    this.preventTouch = this.preventTouch.bind(this);
-    this.touchStart = this.touchStart.bind(this);
+    super(props)
+    this.preventTouch = this.preventTouch.bind(this)
+    this.touchStart = this.touchStart.bind(this)
     this.state = {
       carouselSettings: {
         dots: true,
@@ -68,19 +75,21 @@ class MainSlider extends React.Component {
         swipe: true,
         touchThreshold: 5,
       },
-    };
+    }
   }
 
   componentDidMount() {
     // Disable touchmove to prevent scrolling entire page
-    const carousel = document.getElementById('carousel'); // Your site element containing react-slick's carousel-container
-    carousel.addEventListener('touchstart', this.touchStart);
-    carousel.addEventListener('touchmove', this.preventTouch, { passive: false });
+    const carousel = document.getElementById('carousel') // Your site element containing react-slick's carousel-container
+    carousel.addEventListener('touchstart', this.touchStart)
+    carousel.addEventListener('touchmove', this.preventTouch, {
+      passive: false,
+    })
   }
 
   touchStart(e) {
     // capture user's starting finger position, for later comparison
-    this.firstClientX = e.touches[0].clientX;
+    this.firstClientX = e.touches[0].clientX
   }
 
   preventTouch(e) {
@@ -88,92 +97,63 @@ class MainSlider extends React.Component {
     // this allows the users to scroll vertically past the carousel when touching the carousel
     // this also stabilizes the horizontal scroll somewhat,
     // decreasing vertical scroll while horizontal scrolling
-    const { carouselSettings } = this.state;
-    const clientX = e.touches[0].clientX - this.firstClientX;
-    const horizontalScroll = Math.abs(clientX) > carouselSettings.touchThreshold;
+    const { carouselSettings } = this.state
+    const clientX = e.touches[0].clientX - this.firstClientX
+    const horizontalScroll = Math.abs(clientX) > carouselSettings.touchThreshold
     if (horizontalScroll) {
-      e.preventDefault();
+      e.preventDefault()
     }
   }
 
   render() {
-    const { slides, phone, tablet } = this.props;
-    const { carouselSettings } = this.state;
+    const { slides } = this.props
+    const { carouselSettings } = this.state
 
     // items for slider
-    const items = slides.map((slide) => {
-      let back;
-      if (phone) {
-        back = { background: `url(${slide.primary.image_slide.iphone.url})`, backgroundSize: 'cover' };
-      } else if (tablet) {
-        back = { background: `url(${slide.primary.image_slide.ipad.url})`, backgroundSize: 'cover' };
-      } else {
-        back = { background: `url(${slide.primary.image_slide.url})`, backgroundSize: 'cover' };
-      }
-
-      return (
-        <MainSlide key={slide.primary.title_slideh1[0].text}>
-          <div className="sl" style={back}>
-            <div className="container">
-              <div className="columns">
-                <div className="column is-8-desktop is-offset-1-desktop column is-10-tablet is-offset-2-tablet is-12-mobile">
-                  {RichText.render(slide.primary.title_slideh2, PrismicConfig.linkResolver)}
-                  {RichText.render(slide.primary.title_slideh1, PrismicConfig.linkResolver)}
-                  {RichText.render(slide.primary.description_slide, PrismicConfig.linkResolver)}
-                  <Button
-                    text={slide.primary.button_text_slide}
-                    url={Link.url(slide.primary.button_url_slide, PrismicConfig.linkResolver)}
-                    color="ffffff"
-                  />
-                </div>
+    const items = slides.map(slide => (
+      <MainSlide
+        key={slide.primary.title_slideh1[0].text}
+        back={slide.primary.image_slide}
+      >
+        <div className="sl">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-8-desktop is-offset-1-desktop column is-10-tablet is-offset-2-tablet is-12-mobile">
+                {RichText.render(
+                  slide.primary.title_slideh2,
+                  PrismicConfig.linkResolver
+                )}
+                {RichText.render(
+                  slide.primary.title_slideh1,
+                  PrismicConfig.linkResolver
+                )}
+                {RichText.render(
+                  slide.primary.description_slide,
+                  PrismicConfig.linkResolver
+                )}
+                <Button
+                  text={slide.primary.button_text_slide}
+                  url={Link.url(
+                    slide.primary.button_url_slide,
+                    PrismicConfig.linkResolver
+                  )}
+                  color="ffffff"
+                />
               </div>
             </div>
           </div>
-        </MainSlide>
-      );
-    });
+        </div>
+      </MainSlide>
+    ))
 
     return (
       <SliderSection>
-        <Media
-          query="(min-width: 769px)"
-          defaultMatches={phone === null && tablet === null}
-          render={() => (
-            <div id="carousel">
-              <Slider {...carouselSettings}>
-                {items}
-              </Slider>
-            </div>
-          )
-                          }
-        />
-        <Media
-          query="(min-width: 416px) and (max-width: 768px)"
-          defaultMatches={tablet !== null}
-          render={() => (
-            <div id="carousel">
-              <Slider {...carouselSettings}>
-                {items}
-              </Slider>
-            </div>
-          )
-                          }
-        />
-        <Media
-          query="(max-width: 415px)"
-          defaultMatches={phone !== null}
-          render={() => (
-            <div id="carousel">
-              <Slider {...carouselSettings}>
-                {items}
-              </Slider>
-            </div>
-          )
-                          }
-        />
+        <div id="carousel">
+          <Slider {...carouselSettings}>{items}</Slider>
+        </div>
       </SliderSection>
-    );
+    )
   }
 }
 
-export default MainSlider;
+export default MainSlider
--- components/main/styled/MainSlide.js
+++ components/main/styled/MainSlide.js
@@ -1,83 +1,89 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const MainSlide = styled.div`
-    .sl {
-        height: 65rem;
-        color: white;
-        padding-top: 20rem; 
+  .sl {
+    height: 65rem;
+    color: white;
+    padding-top: 20rem;
+    background: ${props => `url('${props.back.url}')`};
+    background-size: cover;
 
-        h1, h2 {
-            text-transform: uppercase;
-            line-height: 1; 
-        }
-        h1 {
-            font-weight: 900;
-            font-size: 7.2rem; 
-        }
-        h2 {
-            font-weight: normal;
-            font-size: 4.2rem; 
-        }
-        h3 {
-            font-size: 2.2rem;
-            font-weight: normal;
-            margin-top: 2.5rem;
-            line-height: 2.7rem; 
-        }
-        .square_button {
-            height: auto;
-            padding: 1.2rem 4rem;
-            position: relative;
-            left: 0;
-            overflow: hidden;
-            top: 6.5rem;
-            font-size: 1.4rem;
-            text-transform: uppercase; 
-            display:table;
-        }
+    h1,
+    h2 {
+      text-transform: uppercase;
+      line-height: 1;
     }
-    @media (min-width:416px) and (max-width: 768px) {
-        .sl {
-            h1 {
-                font-weight: bold;
-                font-size: 7rem; 
-            }
-            h2 {
-                font-weight: normal;
-                font-size: 4rem; 
-            }
-            h3 {
-                font-weight: normal;
-                font-size: 2.2rem; 
-                line-height: 2.7rem;
-            }
-        }
+    h1 {
+      font-weight: 900;
+      font-size: 7.2rem;
     }
-    @media (max-width: 415px) {
-        .sl {
-            height: 100vh;
-            padding-top: 19rem;
-            
-            h1 {
-                font-weight: bold;
-                font-size: 4rem;
-            }
-            h2 {
-                font-size: 2rem;
-                font-weight: normal;
-            }
-            h3{
-                font-size: 1.8rem;
-                line-height: 2.3rem;
-                font-weight: normal;
-            }
-            .square_button {
-                width: 100%;
-                text-align: center;
-                top: 8rem;
-            }
-        }
+    h2 {
+      font-weight: normal;
+      font-size: 4.2rem;
+    }
+    h3 {
+      font-size: 2.2rem;
+      font-weight: normal;
+      margin-top: 2.5rem;
+      line-height: 2.7rem;
+    }
+    .square_button {
+      height: auto;
+      padding: 1.2rem 4rem;
+      position: relative;
+      left: 0;
+      overflow: hidden;
+      top: 6.5rem;
+      font-size: 1.4rem;
+      text-transform: uppercase;
+      display: table;
+    }
   }
-`;
+  @media (min-width: 416px) and (max-width: 768px) {
+    .sl {
+      background: ${props => `url('${props.back.ipad.url}')`};
 
-export default MainSlide;
+      h1 {
+        font-weight: bold;
+        font-size: 7rem;
+      }
+      h2 {
+        font-weight: normal;
+        font-size: 4rem;
+      }
+      h3 {
+        font-weight: normal;
+        font-size: 2.2rem;
+        line-height: 2.7rem;
+      }
+    }
+  }
+  @media (max-width: 415px) {
+    .sl {
+      height: 100vh;
+      padding-top: 19rem;
+      background: ${props => `url('${props.back.iphone.url}')`};
+
+      h1 {
+        font-weight: bold;
+        font-size: 4rem;
+      }
+      h2 {
+        font-size: 2rem;
+        font-weight: normal;
+      }
+      h3 {
+        font-size: 1.8rem;
+        line-height: 2.3rem;
+        font-weight: normal;
+      }
+      .square_button {
+        width: 100%;
+        text-align: center;
+        top: 8rem;
+      }
+    }
+  }
+`
+
+export default MainSlide
